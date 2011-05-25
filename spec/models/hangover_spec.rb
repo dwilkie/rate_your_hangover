1ac2918ddd2ac49f9f1e17f379501d67e5c8b3bb
David Wilkie <dwilkie@gmail.com>
2011-05-26 06:44:16 +0700

    Added spec for Hangover.of_the_time_period

--- spec/models/hangover_spec.rb
+++ spec/models/hangover_spec.rb
@@ -26,5 +26,68 @@ describe Hangover do
       subject.should respond_to(:votes)
     end
   end
+
+  shared_examples_for(".of_the_time_period") do
+
+    let(:past_hangover) {
+      Factory.create(:hangover, :created_at => 1.send(time_period).ago)
+    }
+
+    context "when a single hangover was created in this time period" do
+      before do
+        hangover.update_attributes!(
+          :created_at => Time.now.utc.send("beginning_of_#{time_period}")
+        )
+      end
+
+      it "should return the hangover" do
+        Hangover.send("of_the_#{time_period}").should == hangover
+      end
+
+      context "and a hangover was created in this time period with 2 votes" do
+        before do
+          2.times do
+            Factory.create(
+              :hangover_vote, :voteable => past_hangover
+            )
+          end
+        end
+
+        it "should return this time period's hangover" do
+          Hangover.send("of_the_#{time_period}").should == hangover
+        end
+      end
+    end
+
+    context "when no hangovers were created in this time period" do
+      it "should return nil" do
+        Hangover.send("of_the_#{time_period}").should be_nil
+      end
+    end
+  end
+
+  describe ".of_the_day" do
+    it_should_behave_like ".of_the_time_period" do
+      let(:time_period) { :day }
+    end
+  end
+
+  describe ".of_the_week" do
+    it_should_behave_like ".of_the_time_period" do
+      let(:time_period) { :week }
+    end
+  end
+
+  describe ".of_the_month" do
+    it_should_behave_like ".of_the_time_period" do
+      let(:time_period) { :month }
+    end
+  end
+
+  describe ".of_the_year" do
+    it_should_behave_like ".of_the_time_period" do
+      let(:time_period) { :year }
+    end
+  end
 end
 
