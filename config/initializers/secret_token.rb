c70da003d27e2c5804b1200d949a21e4aed1521b
David Wilkie <dwilkie@gmail.com>
2011-05-25 16:25:40 +0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RateYourHangover::Application.config.secret_token = '4d02a2e967367c5e7f5e1c7375b127229d37bd820ebc5b8c8b2e1d223bee2e3cf9d19203b1a663d047ef55742b7a5b8c0abfbd1761b97e9558dd898a8b1a0f1a'
