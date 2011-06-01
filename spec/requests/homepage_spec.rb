8198c33e5dca4c5390098455dbaefa2b5c1adf0a
David Wilkie <dwilkie@gmail.com>
2011-06-01 18:10:33 +0700

    fixed bug where test fails when this week is not in this month and added examples for homepage markup

--- spec/requests/homepage_spec.rb
+++ spec/requests/homepage_spec.rb
@@ -7,39 +7,52 @@ def test_hangover_summary_categories(options = {})
   summary_categories.each_with_index do |summary_category, index|
     title = options[summary_category] || options[:title]
 
-    context "within #hangover_#{index + 1}.slide" do
-      let(:slide_selector) { "#hangover_#{index + 1}.slide" }
-       context ".caption p" do
-          it "should show the hangover: \"#{summary_category.to_s.humanize.downcase}\"'s title as: \"#{title}\"" do
-            within "#{slide_selector} .caption p" do
-              page.should have_content(
-                I18n.t("hangover.#{summary_category}", :title => title)
-              )
-            end
+    it "should display the container for the slide" do
+      page.should have_selector "#hangover_#{index + 1}.slide"
+    end
+
+    context "div#hangover_#{index + 1}.slide" do
+      before { parent_selector << "#hangover_#{index + 1}.slide" }
+
+      context "div.caption p" do
+        before { parent_selector << ".caption p" }
+
+        it "should show the hangover: \"#{summary_category.to_s.humanize.downcase}\"'s title as: \"#{title}\"" do
+          within "#{join_parent_selector}" do
+            page.should have_content(
+              I18n.t("hangover.#{summary_category}", :title => title)
+            )
           end
         end
+      end
 
       if options[:image_link] && options[summary_category].nil?
+
         it "should show a link to the hangover" do
-          within "#{slide_selector}" do
+          within "#{join_parent_selector}" do
             page.should have_selector "a[href=\"#{hangover_path(hangover)}\"]"
           end
         end
 
-        it "should show the thumbnail image inside the link" do
-          within "#{slide_selector} a" do
-            page.should have_selector "img[src=\"#{hangover.image_url(:thumb)}\"]"
+        context "a" do
+          before { parent_selector << "a" }
+
+          it "should show the thumbnail image inside the link" do
+            within "#{join_parent_selector}" do
+              page.should have_selector "img[src=\"#{hangover.image_url(:thumb)}\"]"
+            end
           end
         end
+
       else
         it "should not show a link to the hangover" do
-          within "#{slide_selector}" do
+          within "#{join_parent_selector}" do
             page.should_not have_selector "a"
           end
         end
 
         it "should show the thumbnail image" do
-          within "#{slide_selector}" do
+          within "#{join_parent_selector}" do
             page.should have_selector "img[src=\"#{hangover.image_url(:thumb)}\"]"
           end
         end
@@ -52,49 +65,132 @@ describe "Homepage" do
   describe "GET /" do
 
     let(:hangover) { Factory(:hangover) }
+    let(:parent_selector) { [] }
 
-    context "no hangovers exist" do
-      before { visit root_path }
-      test_hangover_summary_categories(
-        :title => I18n.t("hangover.sober"),
-        :image_link => false
-      )
+    def join_parent_selector
+      parent_selector.join(" ")
     end
 
-    sober_periods = {}
-    Hangover::TIME_PERIODS.each_with_index do |time_period, index|
-      context "a hangover exists this #{time_period}" do
-        before do
-          hangover.update_attribute(
-            :created_at, Time.now.utc.send("beginning_of_#{time_period}")
-          )
+    def visit_root_path
+      visit root_path
+    end
+
+    it "should display the hangovers within #hangovers #images" do
+      visit_root_path
+      page.should have_selector("#hangovers #images")
+    end
+
+    context "within" do
+
+      context "div#hangovers div#images" do
+        before { parent_selector << "#hangovers #images" }
+
+        it "should display the frame" do
+          visit_root_path
+          within "#{join_parent_selector}" do
+            page.should have_selector ".frame"
+          end
         end
 
-        if index.zero?
-          before { visit root_path }
-          test_hangover_summary_categories
-        else
-          previous_time_period = Hangover::TIME_PERIODS[index -1]
-          sober_periods[
-            "of_the_#{previous_time_period}".to_sym
-           ] = I18n.t("hangover.sober")
-          context "but not for this #{previous_time_period}" do
-            before do
-              created_at = hangover.created_at
-              utc_time = Time.now.utc
-              hangover.update_attribute(
-                :created_at,
-                created_at.advance(
-                  previous_time_period.to_s.pluralize.to_sym => 1
-                )
-              ) if created_at >= utc_time.send(
-                  "beginning_of_#{previous_time_period}"
-                ) && created_at <= utc_time.send(
-                  "end_of_#{previous_time_period}"
-                )
-              visit root_path
+        it "should display the slides" do
+          visit_root_path
+          within "#{join_parent_selector}" do
+            page.should have_selector "#slides"
+          end
+        end
+
+        context "div#slides" do
+          before { parent_selector << "#slides"}
+          it "should display the slides container" do
+            visit_root_path
+            within "#{join_parent_selector}" do
+              page.should have_selector ".slides_container"
             end
-            test_hangover_summary_categories(sober_periods)
+          end
+
+          it "should display a link to the previous image" do
+            visit_root_path
+            within "#{join_parent_selector}" do
+              page.should have_selector :xpath, './/a[@href="#" and @class="prev"]'
+            end
+          end
+
+          it "should display a link to the next image" do
+            visit_root_path
+            within "#{join_parent_selector}" do
+              page.should have_selector :xpath, './/a[@href="#" and @class="next"]'
+            end
+          end
+
+          context "div.slides_container" do
+            before { parent_selector << ".slides_container" }
+
+            context "no hangovers exist" do
+              before { visit_root_path }
+              test_hangover_summary_categories(
+                :title => I18n.t("hangover.sober"),
+                :image_link => false
+              )
+            end
+
+            sober_periods = {}
+            Hangover::TIME_PERIODS.each_with_index do |time_period, index|
+              context "a hangover exists this #{time_period}" do
+                before do
+                  hangover.update_attribute(
+                    :created_at, Time.now.utc.send("beginning_of_#{time_period}")
+                  )
+                end
+
+                if index.zero?
+                  before { visit_root_path }
+                  test_hangover_summary_categories
+                else
+                  utc_time = Time.now.utc
+                  previous_time_period = Hangover::TIME_PERIODS[index - 1]
+
+                  sober_text = I18n.t("hangover.sober")
+
+                  sober_periods[
+                    "of_the_#{previous_time_period}".to_sym
+                  ] = sober_text
+
+                  temporary_sober_periods = {}
+                  unless time_period == Hangover::TIME_PERIODS.last
+                    next_time_period = Hangover::TIME_PERIODS[index + 1]
+                    temporary_sober_periods[
+                      "of_the_#{next_time_period}".to_sym
+                    ] = sober_text if utc_time.respond_to?(
+                        next_time_period
+                      ) && utc_time.send(
+                        "beginning_of_#{time_period}"
+                      ).send(next_time_period) != utc_time.send(
+                        next_time_period
+                      )
+                  end
+
+                  context "but not for this #{previous_time_period}" do
+                    before do
+                      created_at = hangover.created_at
+                      hangover.update_attribute(
+                        :created_at,
+                        created_at.advance(
+                          previous_time_period.to_s.pluralize.to_sym => 1
+                        )
+                      ) if created_at >= utc_time.send(
+                          "beginning_of_#{previous_time_period}"
+                        ) && created_at <= utc_time.send(
+                          "end_of_#{previous_time_period}"
+                        )
+                      visit_root_path
+                    end
+                    test_hangover_summary_categories(
+                      sober_periods.merge(temporary_sober_periods)
+                    )
+                  end
+                end
+              end
+            end
           end
         end
       end
