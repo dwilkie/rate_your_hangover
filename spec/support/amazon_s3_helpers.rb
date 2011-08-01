04be92ec93ba04ae30b4ee4a73fbdc970985906f
David Wilkie <dwilkie@gmail.com>
2011-08-01 20:30:43 +0700

    Don't require helpers for every type of spec

--- spec/support/amazon_s3_helpers.rb
+++ spec/support/amazon_s3_helpers.rb
@@ -1,60 +1,66 @@
 module AmazonS3Helpers
-  def upload_to_s3(button_text, options = {})
-    options[:success] = true unless options[:success] == false
-    options[:success] &&= !options[:fail]
+  def self.included(base)
+    base.extend ClassMethods
+  end
 
-    if options[:success]
-      # simulate the upload to S3 being successful
+  module ClassMethods
+    def upload_to_s3(button_text, options = {})
+      options[:success] = true unless options[:success] == false
+      options[:success] &&= !options[:fail]
 
-      # form's success action redirect url
-      redirect_url = URI.parse(page.find("input[@name='success_action_redirect']").value)
+      if options[:success]
+        # simulate the upload to S3 being successful
 
-      # form's key
-      key = page.find("input[@name='key']").value
+        # form's success action redirect url
+        redirect_url = URI.parse(page.find("input[@name='success_action_redirect']").value)
 
-      # path of the file to upload
-      upload_path = page.find("input[@name='file']").value
+        # form's key
+        key = page.find("input[@name='key']").value
 
-      # clear all registered urls
-      FakeWeb.clean_registry
+        # path of the file to upload
+        upload_path = page.find("input[@name='file']").value
 
-      if options[:process_image]
-        # attempt to process the image
+        # clear all registered urls
+        FakeWeb.clean_registry
 
-        # build a sample key from the key in the form ignoring the upload path
-        # i.e. simulate a valid file extension upload
-        sample_key = UploaderHelpers.sample_key(:base => key)
+        if options[:process_image]
+          # attempt to process the image
 
-        image_uploader = ImageUploader.new
-        image_uploader.key = sample_key
-        download_url = image_uploader.direct_fog_url(:with_path => true)
+          # build a sample key from the key in the form ignoring the upload path
+          # i.e. simulate a valid file extension upload
+          sample_key = UploaderHelpers.sample_key(:base => key)
 
-        # Register the download url and return the uploaded file in the body
-        FakeWeb.register_uri(:get, download_url, :body => File.open(upload_path))
+          image_uploader = ImageUploader.new
+          image_uploader.key = sample_key
+          download_url = image_uploader.direct_fog_url(:with_path => true)
+
+          # Register the download url and return the uploaded file in the body
+          FakeWeb.register_uri(:get, download_url, :body => File.open(upload_path))
+        else
+          # do not process the image
+
+          # build a key from the key in the form and the upload file path
+          # e.g. to simulate an invalid file extension upload
+          sample_key = UploaderHelpers.sample_key(:base => key, :filename => File.basename(upload_path))
+        end
+
+        redirect_url.query = Rack::Utils.build_nested_query({
+          :bucket => ImageUploader.fog_directory,
+          :key => sample_key,
+          :etag => "\"d41d8cd98f00b204e9800998ecf8427\""
+        })
+
+        # click the button
+        click_button button_text
+
+        # simulate success redirect
+        visit redirect_url.to_s
       else
-        # do not process the image
+        # simulate an unsuccessful s3 upload
 
-        # build a key from the key in the form and the upload file path
-        # e.g. to simulate an invalid file extension upload
-        sample_key = UploaderHelpers.sample_key(:base => key, :filename => File.basename(upload_path))
+        # click the button
+        click_button button_text
       end
-
-      redirect_url.query = Rack::Utils.build_nested_query({
-        :bucket => ImageUploader.fog_directory,
-        :key => sample_key,
-        :etag => "\"d41d8cd98f00b204e9800998ecf8427\""
-      })
-
-      # click the button
-      click_button button_text
-
-      # simulate success redirect
-      visit redirect_url.to_s
-    else
-      # simulate an unsuccessful s3 upload
-
-      # click the button
-      click_button button_text
     end
   end
 end
--- spec/support/request_helpers.rb
+++ spec/support/request_helpers.rb
@@ -1,40 +1,8 @@
-def it_should_show_the_page_title(title)
-  context "the page title" do
-    it "should be '#{title}' in the browser window" do
-      within("title") do
-        page.should have_content title
-      end
-    end
-
-    it "should be '#{title}' in h1" do
-      within("h1") do
-        page.should have_content title
-      end
-    end
-  end
-end
-
-def it_should_display_errors_for(resource, field, message)
-  context field.to_s do
-    it "should show #{spec_translate(message)}" do
-      within(:xpath, ".//input[@id='#{resource}_#{field}']/..") do
-        page.should have_content spec_translate(message)
-      end
-    end
-  end
-end
-
-def it_should_behave_like_clicking_forgot_password
-  context "Clicking the '#{spec_translate(:forgot_password)}' link" do
-    before { click_link spec_translate(:forgot_password) }
-
-    it "should take the user to the forgot password page" do
-      current_path.should == new_user_password_path
-    end
-  end
-end
-
 module RequestHelpers
+  def self.included(base)
+    base.extend ClassMethods
+  end
+
   def sign_in(user)
     visit new_user_session_path
     fill_in 'Email', :with => user.email
@@ -44,6 +12,44 @@ module RequestHelpers
 
   def sign_out
     visit destroy_user_session_path
+  end
+
+  module ClassMethods
+    def it_should_show_the_page_title(title)
+      context "the page title" do
+        it "should be '#{title}' in the browser window" do
+          within("title") do
+            page.should have_content title
+          end
+        end
+
+        it "should be '#{title}' in h1" do
+          within("h1") do
+            page.should have_content title
+          end
+        end
+      end
+    end
+
+    def it_should_display_errors_for(resource, field, message)
+      context field.to_s do
+        it "should show #{spec_translate(message)}" do
+          within(:xpath, ".//input[@id='#{resource}_#{field}']/..") do
+            page.should have_content spec_translate(message)
+          end
+        end
+      end
+    end
+
+    def it_should_behave_like_clicking_forgot_password
+      context "Clicking the '#{spec_translate(:forgot_password)}' link" do
+        before { click_link spec_translate(:forgot_password) }
+
+        it "should take the user to the forgot password page" do
+          current_path.should == new_user_password_path
+        end
+      end
+    end
   end
 end
 
