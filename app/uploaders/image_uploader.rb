c547dd14ac52eb63d16d2520257696784e63e885
David Wilkie <dwilkie@gmail.com>
2011-08-22 15:10:32 +0700

    Moved activemodel and fog call into gem

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -2,13 +2,7 @@
 
 class ImageUploader < CarrierWave::Uploader::Base
   include CarrierWave::RMagick
-
   include CarrierWaveDirect::Uploader
-
-  include ActiveModel::Conversion
-  extend ActiveModel::Naming
-
-  storage :fog
 
   def self.extension_white_list
     %w(jpg jpeg gif png)
